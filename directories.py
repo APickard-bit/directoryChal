fffe02a198c6b7af31f7241b2a623b918dde1d4e
APickard-bit <PandaPickard@gmail.com>
2024-11-09 12:35:50 -0500

    delete

--- directories.py
+++ directories.py
@@ -5,20 +5,34 @@ directoriesFromRoot = {}
 
 # Defining main function
 def main():
-    create("fruit")
-    create("vegetables")
-    create("fruit/apples")
-    create("vegetables/squash/acorns")
+    create("fruit", {})
+    create("vegetables", {})
+    create("fruit/apples", {})
+    create("vegetables/squash/acorns", {})
+    create("vegetables/squash", {})
+    create("vegetables/squash/acorns", {})
+    delete("vegetables/squash/acorns")
     list()
 
 # Create a Directory if it doesn't exist already
-def create(targetPath):
+def create(targetPath, contents):
     dirNames = targetPath.split("/")
+    currentLoc, trail = checkIfTrailIsReal(dirNames)
+    
+    if currentLoc is None:
+        print("Cannot create " + targetPath + " - " + trail + " doesn't exist")
+        return
+
+    targetDirName = dirNames[len(dirNames) - 1]
+    if currentLoc.get(targetDirName, None) is None:
+        currentLoc[targetDirName] = contents
+
+
+# Check the preceeding path trail directories to make sure all the parent directories exist
+# currentLoc will act as the 'pointer' here. Returns the currentLoc dictionary and a string representing it's current trail
+def checkIfTrailIsReal(dirNames): 
     currentLoc = directoriesFromRoot
     index = 1
-
-    # Check the preceeding path trail directories to make sure all the parent directories exist
-    # currentLoc will act as the 'pointer' here
 
     trail = "."
     for dirName in dirNames[0: len(dirNames) - 1: 1]:
@@ -26,13 +40,11 @@ def create(targetPath):
         # If one of the parent dirs doesn't exist then print error and cancel
         currentLoc = currentLoc.get(dirName, None)
         if currentLoc is None:
-            print("Cannot create " + targetPath + " - " + trail + " doesn't exist")
-            return
+            return currentLoc, trail
 
         index = index + 1
 
-    if currentLoc.get(targetPath, None) is None:
-        currentLoc[dirNames[len(dirNames) - 1]] = {}
+    return currentLoc, trail
 
 # List all directories and subdirectories within them. Kicks off listRecur() from the "root" position
 def list():
@@ -53,7 +65,18 @@ def listRecur(currentDir, output, prefix):
 
 # def move(original, targetPath):
 
-# def delete(targetPath):
+def delete(targetPath):
+    dirNames = targetPath.split("/")
+    currentLoc, trail = checkIfTrailIsReal(dirNames)
+
+    if currentLoc is None:
+        print("Cannot delete " + targetPath + " - " + trail + " doesn't exist")
+        return
+
+    targetDirName = dirNames[len(dirNames) - 1]
+    if currentLoc.get(targetDirName, None) is not None:
+        del currentLoc[targetDirName]
+
 
 # Using the special variable 
 # __name__
